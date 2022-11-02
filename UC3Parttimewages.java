5990df587eb6421174e543338a2efa8c6323b437
unknown <chaaudhari.dheeraj12@gmail.com>
2022-11-02 14:07:49 +0530

    Added:Branch UC3 Program to check employee Parrtimewage

---
+++ UC3Parttimewages.java
@@ -0,0 +1,19 @@
+package Day3EmployeeWageProgram;
+
+import java.util.Scanner;
+
+public class UC3Parttimewages {
+	public void parrtime(){
+	Scanner scanner =new Scanner(System.in);
+
+	System.out.println("Enter Per hour wage");
+	double Perhour=scanner.nextDouble();
+	System.out.println("Enter Total hours of working in a Day");
+	double workinghour=scanner.nextDouble(); 
+	System.out.println("Enter  overtime time work hours of employee if not mention zero");
+	double overtimehour=scanner.nextDouble();
+	double dailywage=Perhour*(workinghour+overtimehour);
+	 
+System.out.println("Daily wage is="+dailywage);
+}
+}
